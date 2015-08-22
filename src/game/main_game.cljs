851175d8d1aa8a32ef095191b272b18dd96f9d0d
Antoine Chesnais <achesnais@gmail.com>
2015-08-22 09:12:51 +0200

    World bounds and camera

--- src/game/main_game.cljs
+++ src/game/main_game.cljs
@@ -3,7 +3,7 @@
 (defn MainGame [game]
   (let [still-frame 0
         anim-frame-rate 2
-        speed 1
+        speed 10
 
         dialogue-style (clj->js {:font "20px Arial"
                                  :fill "white"})
@@ -13,12 +13,18 @@
       (create [this]
         (js/console.log "Entering Main Game!")
 
+        ;; Define world bounds
+        (.. game -world (setBounds 0 0 600 700))
+
         ;; Populate world
-        (.. game -add (sprite 0 0 "floor"))
-        (.. game -add (sprite 0 0 "dialogue-box"))
-        (set! (.. this -dialogue) (.. game -add (text 0 0
-                                                      "It begins here"
-                                                      dialogue-style)))
+        (.. game -add (sprite 0 100 "world"))
+        (set! (.. this -dialogue-box) (.. game -add (sprite 0 0 "dialogue-box")))
+        (set! (.. this -dialogue-text)
+              (.. game -add (text (/ (.. game -camera -width) 2)
+                                  50
+                                  "It begins here"
+                                  dialogue-style)))
+        (.. this -dialogue-text -anchor (setTo 0.5 0.5))
         (set! (.. this -player) (.. game -add (sprite 10 10 "player")))
 
         ;; Define animations
@@ -30,6 +36,13 @@
         ;; Set up input
         (set! (.. this -cursors)
               (.. game -input -keyboard (createCursorKeys)))
+
+        ;; Set up camera
+        (.. this -player -anchor (setTo 0.5 0.5))
+        (.. game -camera (follow (.-player this)))
+
+        (set! (.. this -dialogue-box -fixedToCamera) true)
+        (set! (.. this -dialogue-text -fixedToCamera) true)
         )
 
       (update [this]
@@ -40,6 +53,7 @@
               (set! (.. this -player -y)
                     (- y speed)))
             (.. this -player -animations (play "up")))
+
           (.. this -cursors -down -isDown)
           (do
             (let [y  (.. this -player -y)]
@@ -61,4 +75,17 @@
                     (+ x speed)))
             (.. this -player -animations (play "right")))
 
-          :else (set! (.. this -player -frame) still-frame))))))
+          :else (set! (.. this -player -frame) still-frame))
+
+        (.collide this))
+
+      (collide [this]
+        (let [p (.-player this)]
+          (when (< (.-y p) 140)
+            (set! (.-y p) 140))
+          (when (> (.-y p) 660)
+            (set! (.-y p) 660))
+          (when (< (.-x p) 30)
+            (set! (.-x p) 30))
+          (when (> (.-x p) 570)
+            (set! (.-x p) 570)))))))
--- src/game/preload.cljs
+++ src/game/preload.cljs
@@ -14,7 +14,7 @@
 
       ;; Loading all the assets
       (.. game -load (image "dialogue-box" "assets/dialogue-box.png"))
-      (.. game -load (image "floor" "assets/floor.png"))
+      (.. game -load (image "world" "assets/world.png"))
       (.. game -load (image "glass1" "assets/glass1.png"))
       (.. game -load (image "glass2" "assets/glass2.png"))
       (.. game -load (image "glass3" "assets/glass3.png"))
