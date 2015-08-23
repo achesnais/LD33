2a6202372d87c984aca2fe020f41dbc519c3f753
Antoine Chesnais <achesnais@gmail.com>
2015-08-23 23:31:49 +0200

    So thaaaat's what build profiles are for!

--- project.clj
+++ project.clj
@@ -7,14 +7,22 @@
                  [org.clojure/clojurescript "1.7.48"]]
   :plugins [[lein-cljsbuild "1.0.5"]
             [lein-figwheel "0.3.7"]]
+  :hooks [leiningen.cljsbuild]
   :cljsbuild
-  {:builds [{:id "main"
-             :source-paths ["src/"]
+  {:builds {:main
+            {:source-paths ["src/"]
              :figwheel true
              :compiler {:main "game.core"
                         :asset-path "js/out"
                         :output-to "resources/public/js/game.js"
-                        :output-dir "resources/public/js/out"}}]}
+                        :output-dir "resources/public/js/out"}}
+            :final
+            {:source-paths ["src/"]
+             :figwheel false
+             :compiler {:main "game.core"
+                        :asset-path "js/out"
+                        :output-to "target/game.js"
+                        :output-dif "target/js/out"}}}}
   :figwheel
   {:http-server-root "public"
    :server-pot 3449
